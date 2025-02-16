df39b1efb28916c4fbc8a173b9980b57eb04fae6
matiasfiguerero <matiasfiguerero@192.168.1.204>
2025-02-16 16:01:30 -0300

    modificacion prueba

--- app/controllers/hardware_controller.rb
+++ app/controllers/hardware_controller.rb
@@ -5,7 +5,7 @@ class HardwareController < ApplicationController
     @os = OS.report
     @hostname = `hostname`.strip
     @cpu_count = OS.cpu_count
-    @memory = `sysctl -n hw.memsize`.to_i / (1024**3) # Memoria en GB
+    @memory = `sysctl -n hw.memsize`.to_i / (1024**3) # Memoria en GByte
     @disk_info = `df -h`.split("\n").drop(1)
   end
 end
